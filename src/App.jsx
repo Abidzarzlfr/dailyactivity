4746843ce214ebe248ee6897bbac1cf032104b29
Abidzarzlfr <abidzarbijeh@gmail.com>
2025-01-08 17:16:07 +0700

    feat(package, library, src): setup tailwind for development

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
       <h1>{count}</h1>
       <div className="card">
         <button onClick={minus}>-</button>
